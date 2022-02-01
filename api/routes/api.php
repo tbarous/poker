64e6e8aed5e43b3f1b77a4a1d012cced00de363a
Node1 <tasosbarous@gmail.com>
2022-02-01 18:39:22 +0200

    cleanup

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,10 +4,3 @@ use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['XssSanitization']], function () {
-    Route::get('/products', [ProductsController::class, 'filter']);
-    Route::post('/products', [ProductsController::class, 'create']);
-    Route::delete('/products/{id}', [ProductsController::class, 'delete']);
-    Route::post('/products/{id}', [ProductsController::class, 'edit']);
-    Route::get('/report', [ProductsController::class, 'report']);
-});
