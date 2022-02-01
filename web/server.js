cf13f72bc7f0d6ee9f98a1214eb13995bcaf002f
Node1 <tasosbarous@gmail.com>
2022-02-01 18:35:33 +0200

    laravel ide helper

--- web/server.js
+++ web/server.js
@@ -1,11 +1,14 @@
-const express = require('express')
+const express = require('express');
 const app = express()
-const port = 3000
+const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res) {
+    res.sendFile(`${__dirname}/index.html`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- web/src/Styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const H3 = styled.h3`
-    font-family: 'Nunito', sans-serif;
-`;
-
-export {
-    H3
-}
