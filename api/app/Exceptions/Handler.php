c332e64706a6a185609733879542386cf3368b7a
Node1 <tasosbarous@gmail.com>
2022-02-04 17:32:09 +0200

    update postman

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -15,6 +16,14 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         //
     ];
+
+    public function render($request, Exception $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(['message' => 'Not Found'], 404);
+        }
+        return parent::render($request, $exception);
+    }
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
